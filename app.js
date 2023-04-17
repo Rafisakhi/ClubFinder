24ff310a6f75fc47da7b4eee66c5bb4cc75ca463
rafisakhi <rafisakhi57@gmail.com>
2023-04-17 22:52:21 +0700

    Club Finder

---
+++ app.js
@@ -0,0 +1,5 @@
+import "./src/script/component/app-bar";
+import "./src/script/component/search-bar";
+import main from "./src/script/view/main";
+
+document.addEventListener("DOMContentLoaded", main);
